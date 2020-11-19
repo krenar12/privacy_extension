1ee928f36310a0a424595bf9928665d117819192
tirona1 <47080599+tirona1@users.noreply.github.com>
2020-11-19 21:30:38 +0100

    Add files via upload

---
+++ Password Manager/background.js
@@ -0,0 +1,3 @@
+//Background.js file 
+//The background script is the extension's event handler; it contains listeners for browser events that are important to the extension. It lies dormant until an event is fired then performs the instructed logic. An effective background script is only loaded when it is needed and unloaded when it goes idle.
+
